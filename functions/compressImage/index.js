85b6b6d16e5551da4e1ea080edc8bb4ee1bfc54d
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2025-08-03 01:27:52 +0530

    fix: function logic updated

--- functions/compressImage/index.js
+++ functions/compressImage/index.js
@@ -1,10 +1,7 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-
-const sdk = require('node-appwrite');
-const sharp = require('sharp');
-
 module.exports = async function (req, res) {
   try {
+    console.log('Function started - payload:', req.payload);
+
     const client = new sdk.Client()
       .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
       .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
@@ -12,14 +9,28 @@ module.exports = async function (req, res) {
 
     const storage = new sdk.Storage(client);
 
+    if (!req.payload) {
+      throw new Error('No payload provided');
+    }
+
     const { bucketId, fileId } = JSON.parse(req.payload);
+    
+    if (!bucketId || !fileId) {
+      throw new Error('Missing bucketId or fileId in payload');
+    }
+
+    console.log(`Received bucketId: ${bucketId}, fileId: ${fileId}`);
 
     const file = await storage.getFileDownload(bucketId, fileId);
     const buffer = await file.arrayBuffer();
 
+    console.log('File downloaded, size:', buffer.byteLength);
+
     const optimizedBuffer = await sharp(Buffer.from(buffer))
       .jpeg({ quality: 70 })
       .toBuffer();
+
+    console.log('Image compressed, new size:', optimizedBuffer.length);
 
     const uploadResp = await storage.createFile(
       bucketId,
@@ -27,9 +38,11 @@ module.exports = async function (req, res) {
       optimizedBuffer
     );
 
+    console.log('Optimized image uploaded, fileId:', uploadResp.$id);
+
     res.json({ optimizedFileId: uploadResp.$id });
   } catch (error) {
     console.error('Compression function error:', error);
-    res.json({ error: error.message || 'An error occurred' });
+    res.status(500).json({ error: error.message || 'An error occurred' });
   }
-}
+};
